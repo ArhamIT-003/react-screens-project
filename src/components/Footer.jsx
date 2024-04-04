6ced08218565888465fafb146ab2312b18155ba4
Arham <muhammadarham59@gmail.com>
2024-04-04 14:09:18 +0500

    Final Commit and all screens done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,11 @@
 import { FaRegCopyright } from "react-icons/fa6";
+import { useLocation } from "react-router-dom";
 
 const Footer = () => {
+  const { pathname } = useLocation();
   const year = new Date().getFullYear();
+
+  const managerLinks = ["Home", "RFQ Management", "Materials", "User Profile"];
 
   const footerLinks = [
     "Terms of Service",
@@ -9,26 +13,42 @@ const Footer = () => {
     "Contact Us",
     "Help Center",
   ];
+
   return (
-    <div className="h-10 w-full border-t-2 border-gray-400">
-      <div className="flex justify-between items-center px-8 py-2">
-        <div>
-          <p className="text-gray-400 text-sm font-semibold cursor-pointer flex gap-2 items-center">
-            <FaRegCopyright />
-            {year} RFQ Manager. All right reserved.
-          </p>
-        </div>
-        <div className="flex gap-4">
-          {footerLinks.map((item, index) => (
+    <div className="h-10 w-full border-t border-gray-400">
+      {pathname === "/rfq" ||
+        (pathname === "/" && (
+          <div className="flex justify-between items-center px-8 py-2">
+            <div>
+              <p className="text-gray-400 text-sm font-semibold cursor-pointer flex gap-2 items-center">
+                <FaRegCopyright />
+                {year} RFQ Manager. All right reserved.
+              </p>
+            </div>
+            <div className="flex gap-4">
+              {footerLinks.map((item, index) => (
+                <p
+                  key={index}
+                  className="text-gray-400 text-sm font-semibold cursor-pointer"
+                >
+                  {item}
+                </p>
+              ))}
+            </div>
+          </div>
+        ))}
+      {pathname === "/rfq-manager" && (
+        <div className="flex items-center justify-around gap-4 pt-2">
+          {managerLinks.map((item, index) => (
             <p
               key={index}
-              className="text-gray-400 text-sm font-semibold cursor-pointer"
+              className="text-gray-500 text-sm font-semibold cursor-pointer"
             >
               {item}
             </p>
           ))}
         </div>
-      </div>
+      )}
     </div>
   );
 };

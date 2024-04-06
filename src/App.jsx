2c76c05d5e9857c26bdfbb2d366ec897f8d65b20
Arham <muhammadarham59@gmail.com>
2024-04-06 15:18:33 +0500

    Login and Sign-Up screens created

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import SideBar from "./components/SideBar";
 
 // Import your pages
@@ -11,18 +11,23 @@ import Home from "./pages/Home";
 import RFQManager from "./pages/RFQ-Manager";
 import Overview from "./pages/Overview";
 import Instructor from "./pages/Instructor";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
 
 const App = () => {
   const [isOpen, setIsOpen] = useState(false);
-
+  const { pathname } = useLocation();
+  const conditions = pathname === "/login" || pathname === "/signup";
   return (
     <div className="h-screen flex flex-col relative ease-out transition-all ">
-      <Navbar isOpen={isOpen} setIsOpen={setIsOpen} />
+      {!conditions && <Navbar isOpen={isOpen} setIsOpen={setIsOpen} />}
       <div className="ease-out transition-all flex flex-1">
         {isOpen && <SideBar isOpen={isOpen} setOpen={setIsOpen} />}
         <div className={`w-full flex-[8] px-8 ${isOpen ? "ml-52" : ""}`}>
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
             <Route path="/rfq" element={<RFQs />} />
             <Route path="/rfq-manager" element={<RFQManager />} />
             <Route path="/overview" element={<Overview />} />

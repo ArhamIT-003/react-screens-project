2e8ddf8f6c92b8b8aa2e02f876272ef4166915cb
Ali Asghar <aliasghar.chakera@gmail.com>
2024-04-06 15:27:14 +0500

    rfq hook created

---
+++ src/hooks/useRFQ.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const API_URL = process.env.REACT_APP_API_URL;
+
+const useRFQ = (token, rfqId) => {
+  const [rfq, setRFQ] = useState(null);
+
+  useEffect(() => {
+    const fetchRFQ = async () => {
+      try {
+        const response = await axios.get(`${API_URL}/rfq/?rfq_id=${rfqId}`, {
+          headers: { Authorization: `Token ${token}` },
+        });
+        setRFQ(response.data);
+      } catch (error) {
+        console.error('Failed to fetch RFQ:', error);
+      }
+    };
+
+    if (token) {
+      fetchRFQ();
+    }
+  }, [token, rfqId]);
+
+  return rfq;
+}

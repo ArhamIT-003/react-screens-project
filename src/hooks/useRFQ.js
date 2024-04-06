b4eb236578b14805d89a46090bb603997983288e
Ali Asghar <aliasghar.chakera@gmail.com>
2024-04-06 16:28:35 +0500

    login screen integrated

--- src/hooks/useRFQ.js
+++ src/hooks/useRFQ.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = import.meta.env.VITE_API_URL;
 
 const useRFQ = (token, rfqId) => {
   const [rfq, setRFQ] = useState(null);
@@ -25,3 +25,5 @@ const useRFQ = (token, rfqId) => {
 
   return rfq;
 }
+
+export default useRFQ;
